0de0b82589128f0d74d83273f43aeb877ba702e7
Sam Panzer <panzers@seas.upenn.edu>
2012-01-22 01:24:01 -0500

    Added test file and fixed up printing code

--- hw01/sudoku.py
+++ hw01/sudoku.py
@@ -1,39 +1,35 @@
+#!/usr/bin/python
 #Program for solving Sudoku
+
+import string
 class SudokuBoard:
-    
     def __init__(self, inFile):
-        self.board = parseBoard(inFile)
-        self.constraints = computeConstraintsSets()
+        self.board = self.parseBoard(inFile)
+        self.constraints = self.computeConstraintsSets()
         
     def parseBoard(self, inFile):
-        return [[int(symbol) for symbol in line if symbol=='*' symbol = 0] for line in inFile]
+        f = open(inFile, 'r')
+        # Function to replace stars with zeros
+        star = lambda x: int(x) if x != "*" else 0
+        # Note that we strip out the terminating newline from each line.
+        return [[star(symbol) for symbol in line[:-1]] for line in f]
 
     def printBoard(self):
-        for i in range(length(self.board)):
-            result = ''
-            list = self.board[i]
-
-            for j in range(length(list)):
-                if list[j] == 0:
-                    result + '*'
-                else:
-                    result + list[j]
-                if j==2 || j==5:
-                   result + "|"
-
+        star = lambda x: str(x) if x != 0 else "*"
+        box = lambda x: string.join([str(c) for c in x],'')
+        for i in range(len(self.board)):
+            result = [star(c) for c in self.board[i]]
+            result = box(result[:3]) + "|" + box(result[3:6]) + "|" + box(result[-3:])
             print result
-            if i==2 || i==5:
-                print '------+------+------'
+            if i==2 or i==5:
+                print '---+---+---'
 
     def computeConstraintsSets(self):
-        list = []
-
-        for i in range(length(self.board)):
+        constraints = []
+        for i in range(len(self.board)):
             tempList = self.board[i]
 
-            for j in range(length(tempList)):
-                list.append(set([ ]))
-
-        return []
-
-        
+            for j in range(len(tempList)):
+                constraints.append(set([ ]))
+        # Not finished...
+        return constraints
