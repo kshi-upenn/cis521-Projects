706db15f11ad915f567ec27e1dc2222d24acdb0d
Sam Panzer <panzers@seas.upenn.edu>
2012-04-03 22:02:25 -0400

    Stepwise and streamwise classification now work

--- hw7/hw7.py
+++ hw7/hw7.py
@@ -6,7 +6,7 @@ from Dataset import Dataset
 # Bayesian Classifier
 # First, we need to build the probability model
 
-d = Dataset("rec.sport.hockey.txt", "rec.sport.baseball.txt", cutoff=100)
+d = Dataset("rec.sport.hockey.txt", "rec.sport.baseball.txt", cutoff=1000)
 #d = Dataset("comp.sys.mac.hardware.txt", "comp.sys.ibm.pc.hardware.txt", cutoff=2000)
 (Xtrain, Ytrain, Xtest, Ytest) = d.getTrainAndTestSets(0.8, seed=1)
 wordlist = d.getWordList()
@@ -31,11 +31,8 @@ def ridgeTrain(X, Y, l = 1):
   Xt = matrix(X.T)
   return linalg.inv(Xt * matrix(X) + l * identity(X.shape[1])) * Xt * matrix(Y)
 
-def ridgeClassify(x, w):
+def classify(x, w):
   return sign(dot(w, x))
-
-def perceptronClassify(w, x):
-  return sign(dot(w,x))
 
 def perceptronTrain(X,Y, l = 1):
   w = random.rand(X.shape[1])
@@ -46,7 +43,7 @@ def perceptronTrain(X,Y, l = 1):
     iterations += 1
     perfect = True
     for i in range(X.shape[0]):
-      result = perceptronClassify(w, X[i])
+      result = classify(w, X[i])
       if (result != Y[i]):
         perfect = False
         w = w + l * (Y[i] * X[i])
@@ -55,7 +52,7 @@ def perceptronTrain(X,Y, l = 1):
   w = array([average(m[:,i]) for i in range(m.shape[1])])
   return (w, iterations)
 
-def error(w,X,Y,columns, l):
+def l2Error(w,X,Y,columns, l):
    #print (Y)
    #print (Y - dot(w.T, (X[:,columns])[0]))
    s1 = sum([linalg.norm(Y - dot(w.T,x)) for x in X[:,columns]])
@@ -64,34 +61,47 @@ def error(w,X,Y,columns, l):
    s3 = s1 + s2
    return s3
 
+def error(w, X, Y, columns):
+  X = X[:,columns]
+  right = 0
+  wrong = 0
+  for i in range(X.shape[0]):
+    result = classify(w,X[i]) 
+    if result == Y[i]:
+      right += 1
+    else:
+      wrong += 1
+  return wrong
+
 def streamwiseTrain(X, Y, l = 1):
   cols = []
   w = zeros(X.shape[0])
   e = sum([linalg.norm(Y) for x in X])
   for j in range(X.shape[1]):
     print("Now on " + str(j))
-    print("e = " + str(e))
+    # print("e = " + str(e))
     newCols = cols + [j]
     w = ridgeTrain(X[:,newCols], Y, l)
-    curr = error(w,X,Y,newCols, l)
-    print(w)
+    curr = error(w,X,Y,newCols)  + l * linalg.norm(w)
     if curr < e:
-      print("Accepting column " + str(j))
       cols = newCols
       e = curr
-  print cols
+  #print cols
   return (ridgeTrain(X[:,cols],Y,l),cols)
 
-def stepwiseTrain(X, Y, l = 1, maxFeatures = 50):
+def stepwiseTrain(X, Y, l = 1, maxFeatures = 10):
   cols = []
   w = zeros(X.shape[0])
   e = sum([linalg.norm(Y) for x in X])
-  while cols.size < maxFeatures:
+  while len(cols) < maxFeatures:
+    print("Now on " + str(len(cols)))
     best = None
-    for j in (range(X.shape[1]) - cols):
+    for j in range(X.shape[1]):
+      if j in cols:
+        continue
       newCols = cols + [j]
-      w = ridgeTrain(X[:,cols], Y, l)
-      curr = error(w,X,y,cols)
+      w = ridgeTrain(X[:,newCols], Y, l)
+      curr = error(w,X,Y,newCols) + l * linalg.norm(w)
       if curr < e:
         best = j
         e = curr
@@ -101,18 +111,14 @@ def stepwiseTrain(X, Y, l = 1, maxFeatures = 50):
       cols = cols + [best]
   return (ridgeTrain(X[:,cols],Y,l),cols)
 
-# (w, iterations) = perceptronTrain(Xtrain, Ytrain)
-w = ridgeTrain(Xtrain, Ytrain)
-#(w,cols) = streamwiseTrain(Xtrain, Ytrain)
-right = 0
-wrong = 0
-print("Error: ")
-print(error(w,Xtrain, Ytrain, range(Xtrain.shape[1]), 1))
-for i in range(Xtest.shape[0]):
-  #result = perceptronClassify(w,Xtest[i])
-  result = ridgeClassify(w,Xtest[i]) 
-  if result == Ytest[i]:
-    right += 1
-  else:
-    wrong += 1
-print("Right: " + str(right) + "; wrong: " + str(wrong))
+(w, iterations) = perceptronTrain(Xtrain, Ytrain)
+# w = ridgeTrain(Xtrain, Ytrain)
+# (w,cols) = streamwiseTrain(Xtrain, Ytrain)
+# (w,cols) = stepwiseTrain(Xtrain, Ytrain)
+
+e = error(w,Xtest, Ytest, range(Xtest.shape[1]))
+# e = error(w, Xtest, Ytest, cols)
+print("Right: " + str(Ytest.shape[0] - e))
+print("Wrong: " + str(e))
+#print(error(w,Xtrain, Ytrain, range(Xtrain.shape[1])))
+# print("Right: " + str(right) + "; wrong: " + str(wrong))
