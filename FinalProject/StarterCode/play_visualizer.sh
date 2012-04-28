92b1e38a679464d14abcd7d8649235e49d938d2c
Sam Panzer <panzers@seas.upenn.edu>
2012-04-28 13:11:43 -0400

    Added tools directory

--- FinalProject/StarterCode/play_visualizer.sh
+++ FinalProject/StarterCode/play_visualizer.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: play_visualizer.sh <bot1> <bot2>"
+  exit 1
+fi
+
 # Generate a random 2 player map to play on with dims between 20 and 80 
 python src/mapgen.py -n 2 -l 30 -u 80 > src/maps/2player/tmp_random.map
 
